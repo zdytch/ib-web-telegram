6bad01536ffdcc3da460cb0ffe03ddf70de97503
Alexey Luzin <zdytch@gmail.com>
2023-02-02 14:28:57 +0200

    Add ib_connector, get_positions(), _positions_from_ib()

--- app/src/schemas.py
+++ app/src/schemas.py
@@ -3,9 +3,9 @@ from decimal import Decimal
 
 
 class Position(BaseModel):
-    description: str  # "contractDesc":"AAPL"
-    size: int  # "position":100.0
-    market_price: Decimal  # "mktPrice":145.8899994,
-    market_value: Decimal  # "mktValue":14589.0,
-    realized_pnl: Decimal  # "realizedPnl":0.0,
-    unrealized_pnl: Decimal  # "unrealizedPnl":399.33,
+    description: str
+    size: int
+    market_price: Decimal
+    market_value: Decimal
+    realized_pnl: Decimal
+    unrealized_pnl: Decimal
