30ba8e1e5bb3f032339582805af6084b27b23ec0
Alexey Luzin <zdytch@gmail.com>
2023-02-04 21:24:25 +0200

    Add logging

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,14 @@
 from tg_bot import run_tg_bot
+from loguru import logger
+
+logger.add(
+    'logs/{time}.log',
+    format='{time} {level} {message}',
+    level='DEBUG',
+    rotation='100 MB',
+    retention='14 days',
+    compression='zip',
+)
 
 if __name__ == '__main__':
     run_tg_bot()
