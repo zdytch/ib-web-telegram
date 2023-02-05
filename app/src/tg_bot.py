e18dd9cdd21c7beb67b8dd76dc9cd2c70a968c50
Alexey Luzin <zdytch@gmail.com>
2023-02-05 14:14:06 +0200

    Add _get_position_list(), _get_position(),  _positions, _position_expiration

--- app/src/tg_bot.py
+++ app/src/tg_bot.py
@@ -7,6 +7,7 @@ from aiogram.types import (
 )
 from ib_connector import get_positions
 from schemas import Position
+from datetime import datetime, timedelta
 from settings import TELEGRAM_TOKEN
 
 if not TELEGRAM_TOKEN:
@@ -14,6 +15,10 @@ if not TELEGRAM_TOKEN:
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
+
+# TODO: Use db
+_positions: list[Position] = []
+_position_expiration: datetime = datetime.now()
 
 
 def run_tg_bot() -> None:
@@ -27,7 +32,7 @@ async def start(message: Message):
 
 @dp.message_handler(commands=['positions'])
 async def position_list(message: Message):
-    if positions := await get_positions():
+    if positions := await _get_position_list():
         kb = _position_keyboard(positions)
 
         await message.answer(f'Total Positions: {len(positions)}', reply_markup=kb)
@@ -43,7 +48,28 @@ async def unknown_message(message: Message):
 
 @dp.callback_query_handler()
 async def inline_callback(callback: CallbackQuery):
-    await callback.message.answer(f'{callback.data} Position Details')
+    if position := await _get_position(callback.data):
+        await callback.message.answer(f'{position}')
+
+
+async def _get_position_list() -> list[Position]:
+    global _positions
+    global _position_expiration
+
+    if (now := datetime.now()) >= _position_expiration:
+        _positions.clear()
+
+        if origin_positions := await get_positions():
+            _positions = origin_positions
+            _position_expiration = now + timedelta(seconds=60)
+
+    return _positions
+
+
+async def _get_position(description: str) -> Position | None:
+    positions = await _get_position_list()
+
+    return next((p for p in positions if p.description == description), None)
 
 
 def _position_keyboard(positions: list[Position]) -> InlineKeyboardMarkup:
