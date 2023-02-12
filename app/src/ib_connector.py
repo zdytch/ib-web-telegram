52570de7c1590e2d552ed2757e397e9c9f243d84
Alexey Luzin <zdytch@gmail.com>
2023-02-12 17:55:00 +0200

    Use _send_request() for get_orders(), get_order(), cancel_order()

--- app/src/ib_connector.py
+++ app/src/ib_connector.py
@@ -1,8 +1,13 @@
+from asyncio import sleep
 from httpx import AsyncClient, HTTPError, codes
 from schemas import Position, Order, Side, OrderStatus, OrderType
 from decimal import Decimal
 from loguru import logger
 from settings import IB_URL_BASE
+
+
+class IBConnectorError(Exception):
+    pass
 
 
 async def get_positions() -> list[Position]:
@@ -34,62 +39,53 @@ async def get_position(contract_id: int) -> Position | None:
 
 
 async def get_orders() -> list[Order]:
-    orders = []
+    '''
+    https://interactivebrokers.github.io/cpwebapi/use-cases
+    Similarly to market data snapshots, order updates retrieved via the
+    /iserver/account/orders endpoints will require two calls to the server before
+    meaningful data is returned. The first call creates a subscription to the order
+    updates, with the second call returning the actual data. Please allow up to 5 seconds
+    for the order updates subscription to be created before making the second call.
+    '''
 
-    try:
-        async with AsyncClient(verify=False) as client:
-            # TODO: Investigate
-            await client.get(f'{IB_URL_BASE}/iserver/accounts')
+    data = await _send_request(
+        'GET',
+        '/iserver/account/orders',
+        {'Filters': 'pending_submit,pre_submitted,submitted'},
+    )
 
-            response = await client.get(
-                f'{IB_URL_BASE}/iserver/account/orders?Filters=pending_submit,pre_submitted,submitted'
-            )
+    if isinstance(data, dict) and not data['snapshot']:
+        await sleep(1)
 
-        if response.status_code == codes.OK:
-            orders = _orders_from_ib(response.json()['orders'])
+        # TODO Recursion
+        data = await get_orders()
 
-    except HTTPError as error:
-        logger.debug(error)
+    if isinstance(data, dict):
+        return _orders_from_ib(data['orders'])
 
-    return orders
+    else:
+        raise IBConnectorError(f'Cannot get order list, wrong value returned: {data}')
 
 
 async def get_order(id: int) -> Order | None:
-    order = None
+    data = await _send_request('GET', f'/iserver/account/order/status/{id}')
 
-    try:
-        async with AsyncClient(verify=False) as client:
-            # TODO: Investigate
-            await client.get(f'{IB_URL_BASE}/iserver/accounts')
+    if isinstance(data, dict):
+        return _order_from_ib(data) if not data.get('error') else None
 
-            response = await client.get(
-                f'{IB_URL_BASE}/iserver/account/order/status/{id}'
-            )
-
-        if response.status_code == codes.OK:
-            order = _order_from_ib(response.json())
-
-    except HTTPError as error:
-        logger.debug(error)
-
-    return order
+    else:
+        raise IBConnectorError(f'Cannot get order, wrong value returned: {data}')
 
 
 async def cancel_order(id: int) -> None:
-    try:
-        async with AsyncClient(verify=False) as client:
-            # TODO: Investigate
-            await client.get(f'{IB_URL_BASE}/iserver/accounts')
+    '''
+    https://interactivebrokers.github.io/cpwebapi/endpoints
+    Cancels an open order. Must call /iserver/accounts endpoint prior to cancelling an order.
+    Use /iservers/account/orders endpoint to review open-order(s) and get latest order status.
+    '''
+    await _send_request('GET', '/iserver/accounts')
 
-            response = await client.delete(
-                f'{IB_URL_BASE}/iserver/account/DU1692823/order/{id}'
-            )
-
-        if response.status_code == codes.OK:
-            pass
-
-    except HTTPError as error:
-        logger.debug(error)
+    await _send_request('DELETE', f'/iserver/account/DU1692823/order/{id}')
 
 
 async def cancel_all_orders() -> None:
@@ -97,6 +93,19 @@ async def cancel_all_orders() -> None:
 
     for order in orders:
         await cancel_order(order.id)
+
+
+async def _send_request(method: str, endpoint: str, params: dict = {}) -> dict | list:
+    try:
+        async with AsyncClient(verify=False) as client:
+            r = await client.request(method, f'{IB_URL_BASE}{endpoint}', params=params)
+
+        return r.json()
+
+    except HTTPError as error:
+        logger.error(error)
+
+        raise IBConnectorError()
 
 
 def _positions_from_ib(ib_positions: list[dict]) -> list[Position]:
